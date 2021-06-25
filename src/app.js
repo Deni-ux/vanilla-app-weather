7c428dcf8c9b1fb894abf9006a3749a5b31b261b
Denisa Haka <denisa.haka@ymail.com>
2021-06-25 23:14:46 +0100

    new work done

---
+++ src/app.js
@@ -0,0 +1,106 @@
+function formatDate(timestamp){
+  let date = new Date(timestamp);
+let hours=date.getHours();
+if (hours<10) {
+    hours=`0${hours}`;
+}
+let minutes=date.getMinutes();
+if (minutes<10) {
+    minutes=`0${minutes}`;
+}
+let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let day =days[date.getDay()];
+
+return `${day}, ${hours}:${minutes}`;  
+}
+let currentDate=document.querySelector("#date");
+currentDate.innerHTML=formatDate(new Date);
+
+function displayForecast(){
+    let forecastElement=document.querySelector("#forecast");
+    forecastElement.innerHTML="Forecast";
+
+}
+
+
+function displayTemperature(response){
+    document.querySelector("#city").innerHTML=response.data.name;
+    celsiusTemperature=Math.round(response.data.main.temp);
+   
+    document.querySelector("#temperature").innerHTML=celsiusTemperature;
+    document.querySelector("#humidity").innerHTML=response.data.main.humidity;
+    document.querySelector("#windspeed").innerHTML=Math.round(response.data.wind.speed);
+    
+    document.querySelector("#weatherDescription").innerHTML=`Currently ${response.data.weather[0].description}`;
+    let iconElement = document.querySelector("#weather-icon");
+    iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+ 
+  
+ } 
+
+function searchCity(city){
+   let apiKey = "b2694a5d8f39bb351277f910bc5d27c4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(displayTemperature);
+}
+
+function showCities(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-input");
+    searchCity(searchInput.value);
+}
+let form =document.querySelector("#search-form");
+form.addEventListener("submit",showCities);
+ 
+function showPosition(position) {
+  let apiKey = "b2694a5d8f39bb351277f910bc5d27c4";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getLocalPosition(event) {
+  event.preventDefault();
+
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.querySelector("#current-location");
+button.addEventListener("click", getLocalPosition);
+
+function displayFahrenheitTemp(event){
+    event.preventDefault();
+    let tempElement=document.querySelector("#temperature");
+    let fahrenheitTemp=Math.round((celsiusTemperature*9)/5+32);
+    tempElement.innerHTML=fahrenheitTemp;
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+
+}
+let fahrenheitLink=document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click",displayFahrenheitTemp);
+
+function displayCelsiusTemp(event){
+    event.preventDefault();
+    let tempElement=document.querySelector("#temperature");
+    tempElement.innerHTML=celsiusTemperature;
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+}
+let celsiusLink=document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click",displayCelsiusTemp);
+
+
+let celsiusTemperature=null;
+
+
+
+
+
+searchCity("London");
+displayForecast(); 
